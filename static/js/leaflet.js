cdbc353880f3c9791ab24de696cad8c0ceb0c86d
fabriziofrocio <116425701+fabriziofrocio@users.noreply.github.com>
2023-09-21 09:52:43 +0200

    1

---
+++ static/js/leaflet.js
@@ -0,0 +1,41 @@
+function createMap(lat, lng) {
+    let latinput = document.querySelector("#lat");
+    let lnginput = document.querySelector("#lng");
+
+    var map = L.map("map").setView([lat, lng], 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    var popup = L.popup();
+    var marker = L.marker();
+
+    function onMapClick(e) {
+        map.removeLayer(marker);
+        marker = L.marker(e.latlng).addTo(map);
+
+        popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(map);
+
+        let lat = e.latlng.lat.toString();
+        let lng = e.latlng.lng.toString();
+
+        latinput.value = lat;
+        lnginput.value = lng;
+    }
+
+    map.on("click", onMapClick);
+}
+navigator.geolocation.getCurrentPosition(
+
+    function (event) {
+        createMap(event.coords.latitude, event.coords.longitude)
+    }, function (event) {
+        createMap(51, - 0.09)
+    }
+)
